19802fc969cfa2007b054ee7de636d6a77d84d75
toy <devluisteran@gmail.com>
2025-06-24 23:33:09 -0600

    new profile

---
+++ script.js
@@ -0,0 +1,11 @@
+// Efecto smooth scroll para los links del navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Puedes añadir más funcionalidades aquí (ej: animaciones)
